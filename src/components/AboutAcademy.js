4a3a30a23464c716ebebcccd69c5ec0f11628cd2
mohinderps <mohin19derp@gmail.com>
2019-07-23 16:29:56 +0530

    Adds About Academy

---
+++ src/components/AboutAcademy.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const AboutAcademy = () => (
+  <div>
+    Bhangra Sway is the only academy in Delhi NCR that focuses on training of
+    traditional dances of punjab. We have well qualified trainers from Punjab
+    region. We have weekday and weekend batches. We also provide personal
+    sessions.
+  </div>
+)
