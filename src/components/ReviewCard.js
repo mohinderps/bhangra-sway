d98bc4d934943fbe0dfd311d7648e806c23b4905
mohinderps <mohin19derp@gmail.com>
2019-07-30 18:28:43 +0530

    Updates reviews typography

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -4,7 +4,12 @@ import { QuoteAltLeft } from "styled-icons/boxicons-solid/QuoteAltLeft"
 
 const ReviewCard = ({ reviewedBy, reviewText }) => (
   <Card bg="#fff" p={40} css={{ height: "100%", position: "relative" }}>
-    <Box pl={40} mb={30} css={{ position: "relative" }}>
+    <Box
+      pl={40}
+      mb={30}
+      color="#999"
+      css={{ position: "relative", "letter-spacing": "0.025em" }}
+    >
       <Box css={{ position: "absolute", top: "0", left: "0" }}>
         <QuoteAltLeft width={32} height={32} />
       </Box>
